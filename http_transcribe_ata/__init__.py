3ca5da8ae86ddc11f1ddb796e769f074aceb24d9
João Arantes <arantesjoao0405@gmail.com>
2025-09-10 14:09:33 -0300

    Update __init__.py

--- http_transcribe_ata/__init__.py
+++ http_transcribe_ata/__init__.py
@@ -7,17 +7,18 @@ from datetime import datetime
 from urllib.parse import urlparse
 import requests
 from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, ContentSettings
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Testando azure.identity')
+    logging.info('Testando azure.storage.blob')
     
     try:
         if req.params.get("ping") == "1":
             return func.HttpResponse(
                 json.dumps({
                     "status": "success", 
-                    "message": "Azure Identity OK",
-                    "tested": ["basic_imports", "azure.identity"]
+                    "message": "Azure Storage OK",
+                    "tested": ["basic_imports", "azure.identity", "azure.storage.blob"]
                 }),
                 mimetype="application/json",
                 status_code=200
